72fa055562714f163dffa4c694733e1fe0dc6355
Hussie <husnainkhaliq82@gmail.com>
2024-09-16 19:00:39 -0400

    Added chatbot

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Chatbot from './Chatbot';
+import Chatbot from './chatbot';
 import { GithubIcon } from 'lucide-react';
 
 // Create a modern theme with blue and teal tones
